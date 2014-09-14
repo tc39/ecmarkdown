d9227a1019201a99b8b2e3d5d7c7cfea5255b838
Domenic Denicola <domenic@domenicdenicola.com>
2014-09-14 12:45:19 +0200

    De-Traceurify :(
    Since PEGjs is not Traceur-aware, we were only able to use ES6 in the tiniest parts of the code, so it wasn't worth it. This could be reversed in the future with a PEGjs plugin (like pegcoffee), but for now let's go back to ES5.

--- test/run-cases.js
+++ test/run-cases.js
@@ -1,22 +1,22 @@
-var assert = require("assert");
-var fs = require("fs");
-var glob = require("glob");
-var path = require("path");
-
-import generate from "..";
+'use strict';
+var assert = require('assert');
+var fs = require('fs');
+var glob = require('glob');
+var path = require('path');
+var generate = require('..');
 
 // TODO: dedupe this framework with domenic/webidl-class-generator. Inputs: .markdown, .html
 
-describe("Checking inputs against outputs", () => {
-    glob.sync(path.resolve(__dirname, "cases/*.ecmarkdown")).forEach(ecmarkdownFilePath => {
+describe('Checking inputs against outputs', function () {
+    glob.sync(path.resolve(__dirname, 'cases/*.ecmarkdown')).forEach(function (ecmarkdownFilePath) {
         var ecmarkdownFileName = path.basename(ecmarkdownFilePath);
-        var htmlFileName = path.basename(ecmarkdownFilePath, ".ecmarkdown") + ".html";
-        var htmlFilePath = path.resolve(__dirname, "cases", htmlFileName);
+        var htmlFileName = path.basename(ecmarkdownFilePath, '.ecmarkdown') + '.html';
+        var htmlFilePath = path.resolve(__dirname, 'cases', htmlFileName);
 
-        var ecmarkdownContents = fs.readFileSync(ecmarkdownFilePath, { encoding: "utf-8" }).trim();
-        var htmlContents = fs.readFileSync(htmlFilePath, { encoding: "utf-8" }).trim();
+        var ecmarkdownContents = fs.readFileSync(ecmarkdownFilePath, { encoding: 'utf-8' }).trim();
+        var htmlContents = fs.readFileSync(htmlFilePath, { encoding: 'utf-8' }).trim();
 
-        specify(ecmarkdownFileName, () => {
+        specify(ecmarkdownFileName, function () {
             assert.strictEqual(generate(ecmarkdownContents), htmlContents);
         });
     });
