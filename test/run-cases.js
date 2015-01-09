13d74e87a2781352ca7a37f9ecf96b29c99e17d3
Domenic Denicola <d@domenic.me>
2015-01-09 14:57:03 -0500

    Use baseline-tester to test

--- test/run-cases.js
+++ test/run-cases.js
@@ -1,23 +1,10 @@
 'use strict';
-var assert = require('assert');
-var fs = require('fs');
-var glob = require('glob');
 var path = require('path');
+var baselineTester = require('baseline-tester');
 var generate = require('..');
 
-// TODO: dedupe this framework with domenic/webidl-class-generator. Inputs: .markdown, .html
-
-describe('Checking inputs against outputs', function () {
-    glob.sync(path.resolve(__dirname, 'cases/*.ecmarkdown')).forEach(function (ecmarkdownFilePath) {
-        var ecmarkdownFileName = path.basename(ecmarkdownFilePath);
-        var htmlFileName = path.basename(ecmarkdownFilePath, '.ecmarkdown') + '.html';
-        var htmlFilePath = path.resolve(__dirname, 'cases', htmlFileName);
-
-        var ecmarkdownContents = fs.readFileSync(ecmarkdownFilePath, { encoding: 'utf-8' }).trim();
-        var htmlContents = fs.readFileSync(htmlFilePath, { encoding: 'utf-8' }).trim();
-
-        specify(ecmarkdownFileName, function () {
-            assert.strictEqual(generate(ecmarkdownContents), htmlContents);
-        });
-    });
+baselineTester(generate, {
+    casesDirectory: path.resolve(__dirname, 'cases'),
+    inputExtension: 'ecmarkdown',
+    outputExtension: 'html'
 });
