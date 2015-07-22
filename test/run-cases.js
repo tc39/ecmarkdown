cb34ebab3be8121e5184e87a4ae8aa04d485c0ee
Brian Terlson <brian.terlson@microsoft.com>
2015-07-22 16:17:40 -0700

    Move parser to separate file and fix lint errors

--- test/run-cases.js
+++ test/run-cases.js
@@ -9,20 +9,20 @@ Bluebird.try(function () {
     casesDirectory: path.resolve(__dirname, 'list-cases'),
     inputExtension: 'ecmarkdown',
     outputExtension: 'html'
-  })
+  });
 })
 .then(function () {
   return baselineTester(ecmarkdown.document, {
     casesDirectory: path.resolve(__dirname, 'paragraph-cases'),
     inputExtension: 'ecmarkdown',
     outputExtension: 'html'
-  })
+  });
 })
 .then(function () {
   return baselineTester(ecmarkdown.fragment, {
     casesDirectory: path.resolve(__dirname, 'fragment-cases'),
     inputExtension: 'ecmarkdown',
     outputExtension: 'html'
-  })
+  });
 })
 .done();
