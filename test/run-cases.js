950c72f7095e91f2fa789ed6a463e8d11d1ddc4d
Domenic Denicola <d@domenic.me>
2015-07-24 10:18:47 -0400

    Remove beautification
    Closes #12. The tests keep doing beautification though, as explained in a comment in run-cases.js.

--- test/run-cases.js
+++ test/run-cases.js
@@ -2,27 +2,39 @@
 const path = require('path');
 const Bluebird = require('bluebird');
 const baselineTester = Bluebird.promisify(require('baseline-tester'));
+const beautify = require('./helpers/beautify.js');
+
 const ecmarkdown = require('..');
 
 Bluebird.try(function () {
-  return baselineTester(ecmarkdown.document, {
+  return baselineTester(beautified(ecmarkdown.document), {
     casesDirectory: path.resolve(__dirname, 'list-cases'),
     inputExtension: 'ecmarkdown',
     outputExtension: 'html'
   });
 })
 .then(function () {
-  return baselineTester(ecmarkdown.document, {
+  return baselineTester(beautified(ecmarkdown.document), {
     casesDirectory: path.resolve(__dirname, 'paragraph-cases'),
     inputExtension: 'ecmarkdown',
     outputExtension: 'html'
   });
 })
 .then(function () {
-  return baselineTester(ecmarkdown.fragment, {
+  return baselineTester(beautified(ecmarkdown.fragment), {
     casesDirectory: path.resolve(__dirname, 'fragment-cases'),
     inputExtension: 'ecmarkdown',
     outputExtension: 'html'
   });
 })
 .done();
+
+function beautified(fn) {
+  // In order to be able to read the test case outputs, we write them with nice linebreaks and spacing.
+  // However, Ecmarkdown does not output such beautiful HTML, largely for speed reasons (and also it is probably
+  // redundant as you would instead beautify the outer document). Thus, we beautify the Ecmarkdown output before
+  // comparing it to the test case outputs.
+  return function () {
+    return beautify(fn.apply(undefined, arguments));
+  };
+}
