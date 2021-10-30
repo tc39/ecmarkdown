22d7d6e7d919554c3b6942e56f418262dc54ff8f
Kevin Gibbons <bakkot@gmail.com>
2021-10-30 15:14:38 -0700

    more readable assertsions for location

--- test/parser.js
+++ test/parser.js
@@ -3,42 +3,19 @@ const assert = require('assert');
 const { parseAlgorithm } = require('../');
 
 describe('Parser', function () {
-  function assertNodeLocation(node, name, location) {
-    assert.equal(node.name, name);
-    assert.deepEqual(node.location, location);
-  }
-  function stripLocations(node) {
-    return JSON.parse(JSON.stringify(node, (k, v) => (k === 'location' ? undefined : v)));
-  }
   it('tracks positions', function () {
-    const algorithm = parseAlgorithm('  1. [id="thing"] a\n  2. b c');
-    assertNodeLocation(algorithm, 'algorithm', {
-      start: { line: 1, column: 1, offset: 0 },
-      end: { line: 2, column: 9, offset: 28 },
-    });
+    const baseSource = '  1. [id="thing"] a\n  2. b c';
+    const assertNodeLocation = makeAssertLocation(baseSource);
+    const algorithm = parseAlgorithm(baseSource);
+    assertNodeLocation(algorithm, baseSource);
     const list = algorithm.contents;
-    assertNodeLocation(list, 'ol', {
-      start: { line: 1, column: 1, offset: 0 },
-      end: { line: 2, column: 9, offset: 28 },
-    });
+    assertNodeLocation(list, '  1. [id="thing"] a\n  2. b c');
     const item0 = list.contents[0];
-    assertNodeLocation(item0, 'ordered-list-item', {
-      start: { line: 1, column: 1, offset: 0 },
-      end: { line: 2, column: 1, offset: 20 },
-    });
-    assertNodeLocation(item0.contents[0], 'text', {
-      start: { line: 1, column: 19, offset: 18 },
-      end: { line: 1, column: 20, offset: 19 },
-    });
+    assertNodeLocation(item0, '  1. [id="thing"] a\n');
+    assertNodeLocation(item0.contents[0], 'a');
     const item1 = list.contents[1];
-    assertNodeLocation(item1, 'ordered-list-item', {
-      start: { line: 2, column: 1, offset: 20 },
-      end: { line: 2, column: 9, offset: 28 },
-    });
-    assertNodeLocation(item1.contents[0], 'text', {
-      start: { line: 2, column: 6, offset: 25 },
-      end: { line: 2, column: 9, offset: 28 },
-    });
+    assertNodeLocation(item1, '  2. b c');
+    assertNodeLocation(item1.contents[0], 'b c');
   });
 
   it('does not consider comments to be text', function () {
@@ -74,3 +51,43 @@ describe('Parser', function () {
     });
   });
 });
+
+// this gives one-based line and column numbers
+function offsetToLineAndColumn(string, offset) {
+  const lines = string.split('\n');
+  let line = 0;
+  let seen = 0;
+  while (true) {
+    if (line >= lines.length) {
+      throw new Error(`offset ${offset} exceeded string ${JSON.stringify(string)}`);
+    }
+    if (seen + lines[line].length >= offset) {
+      break;
+    }
+    seen += lines[line].length + 1; // +1 for the '\n'
+    ++line;
+  }
+  const column = offset - seen;
+  return { line: line + 1, column: column + 1 };
+}
+
+function stripLocations(node) {
+  return JSON.parse(JSON.stringify(node, (k, v) => (k === 'location' ? undefined : v)));
+}
+
+function makeAssertLocation(baseSource) {
+  return function assertNodeLocation(node, nodeSource) {
+    let { start, end } = node.location;
+    assert.strictEqual(baseSource.substring(start.offset, end.offset), nodeSource);
+    // prettier-ignore
+    let { line: startLine, column: startColumn } =
+    offsetToLineAndColumn(baseSource, start.offset);
+    assert.strictEqual(start.line, startLine);
+    assert.strictEqual(start.column, startColumn);
+    // prettier-ignore
+    let { line: endLine, column: endColumn } =
+    offsetToLineAndColumn(baseSource, end.offset);
+    assert.strictEqual(end.line, endLine);
+    assert.strictEqual(end.column, endColumn);
+  };
+}
